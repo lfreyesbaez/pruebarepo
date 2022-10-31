f8449a84ba639285a65426c60dc18cc193c2ad07
Luis Reyes <lfreyes@humano.com.do>
2022-10-31 17:44:51 -0400

    nuevo repositorio

---
+++ postAttachment.php
@@ -0,0 +1,19 @@
+<?php 
+
+$path = 'C:/xampp/htdocs/prueba/prueba';
+$uploadFile = $path. basename($_FILES['archivo']['name']);
+
+if (move_uploaded_file($_FILES['archivo']['name'], $uploadFile)) {
+    echo 'File uploaded successfully'; 
+}else {
+    echo 'File not uploaded'. $_FILES['archivo']['name'];
+}
+
+echo '<br>';
+print_r($_FILES['archivo']['name']);
+
+echo '<br>';
+print_r(move_uploaded_file($_FILES['archivo']['name'], $uploadFile));
+
+?>
+
